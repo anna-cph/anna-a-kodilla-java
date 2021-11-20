d7b47f9560679b279144306a3b85c47a585c342d
Anna <annn@g.pl>
2021-11-20 18:55:39 +0100

    6.2 TDD updated

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,22 +1,34 @@
 package com.kodilla.testing;
 
 import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args) {
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+
         Calculator calculator = new Calculator(12, 5);
 
-        int result = calculator.add();
+        int result1 = calculator.add();
 
-        if (result==17) {
+        if (result1==17) {
             System.out.println("Test for adding- OK");
         } else {
             System.out.println("Test for adding- Error!!");
         }
 
-        int result1 = calculator.subtract();
+        int result2 = calculator.subtract();
 
-        if (result1==7) {
+        if (result2==7) {
             System.out.println("Test for subtracting- OK");
         } else {
             System.out.println("Test for subtracting- Error!!");
