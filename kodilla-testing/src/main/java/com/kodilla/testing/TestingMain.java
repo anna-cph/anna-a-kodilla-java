b5d3e80919cddbcfa250272e27a4316b3e577efe
Anna <annn@g.pl>
2021-11-20 18:06:49 +0100

    6.2 Test Driven Development

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,28 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        Calculator calculator = new Calculator(12, 5);
+
+        int result = calculator.add();
+
+        if (result==17) {
+            System.out.println("Test for adding- OK");
+        } else {
+            System.out.println("Test for adding- Error!!");
+        }
+
+        int result1 = calculator.subtract();
+
+        if (result1==7) {
+            System.out.println("Test for subtracting- OK");
+        } else {
+            System.out.println("Test for subtracting- Error!!");
+        }
+
     }
+
 }
+
