2e78812a2509eaf9b974dcadc1ba200c7ff4e9d6
Anna <annn@g.pl>
2022-01-16 16:32:02 +0100

    8.1 error

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,2 +1,13 @@
-package com.kodilla.exception.main;public class ExceptionModuleRunner {
+package com.kodilla.exception.main;
+
+import com.kodilla.exception.io.FileReader;
+
+public class ExceptionModuleRunner {
+
+    public static void main(String[] args) {
+        FileReader fileReader = new FileReader();
+        fileReader.readFile();
+    }
 }
+
+
