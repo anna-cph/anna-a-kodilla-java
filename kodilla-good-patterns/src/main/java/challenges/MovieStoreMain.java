8476d05f24e3661394ddca6339b81381f2e397df
Anna <annn@g.pl>
2022-01-25 16:53:31 +0100

    6.1 poprawka

--- kodilla-good-patterns/src/main/java/challenges/MovieStoreMain.java
+++ kodilla-good-patterns/src/main/java/challenges/MovieStoreMain.java
@@ -2,14 +2,17 @@ package challenges;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class MovieStoreMain {
     public static void main(String[] args) {
         MovieStore theMovie = new MovieStore();
         Map<String, List<String>> theMovieMap = theMovie.getMovies();
         System.out.println("The movie titles:");
-        theMovieMap.entrySet().stream()
-                .map(entry -> entry.getValue() + " !")
-                .forEach(System.out::println);
+        String xx = theMovieMap.entrySet().stream()
+                .map(entry -> entry.getValue())
+                .map(entry -> entry.toString())
+                .collect(Collectors.joining(" ! \n","",""));
+                System.out.println(xx);
     }
 }
