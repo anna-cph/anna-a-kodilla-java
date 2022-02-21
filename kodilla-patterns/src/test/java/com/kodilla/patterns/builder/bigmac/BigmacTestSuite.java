120b479778dfb9a365d14db8c68ad3a262112e61
Anna <annn@g.pl>
2022-02-21 22:17:57 +0100

    10.5 builder

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BigmacTestSuite {
+    @Test
+    void testBigmacNew() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .ingredient("Onion")
+                .burgers(2)
+                .bun("With sesame")
+                .sauce("Spicy")
+                .ingredient("Pickle")
+                .ingredient("Bacon")
+                .build();
+        System.out.println(bigmac);
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+        //Then
+        assertEquals(3, howManyIngredients);
+    }
+}
